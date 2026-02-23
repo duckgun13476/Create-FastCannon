952f52640eb42b73aee0eadcf7a2457d745d9761
Pink_Cats <1907284584@qq.com>
2026-02-23 13:22:48 +1100

    Fix run problem for 1.19.2.

--- src/main/java/com/Pink_Cats/createfastschematiccannon/Config.java
+++ src/main/java/com/Pink_Cats/createfastschematiccannon/Config.java
@@ -13,7 +13,7 @@ import java.util.Set;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Forge's config APIs
-@Mod(Createfastschematiccannon.MODID)
+@Mod.EventBusSubscriber(modid = Createfastschematiccannon.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Config {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
